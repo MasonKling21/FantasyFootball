082b2a9ffeaeba3dceaa2db72ce89b566200fa1b
Mason Kling <klingma@oregonstate.edu>
2022-10-19 11:57:23 -0700

    Added secure logins

--- JavaServer/src/main/java/com/example/FantasyFootball/User.java
+++ JavaServer/src/main/java/com/example/FantasyFootball/User.java
@@ -4,11 +4,13 @@ public class User {
 	private final String username;
 	private final String email;
 	private final String password;
+	private final String salt;
 
-	public User(String username, String email, String password) {
+	public User(String username, String email, String password, String salt) {
 		this.username = username;
 		this.email = email;
 		this.password = password;
+		this.salt = salt;
 	}
 
 	public String getUsername() {
@@ -21,6 +23,10 @@ public class User {
 
 	public String getPassword() {
 		return this.password;
+	}
+
+	public String getSalt() {
+		return this.salt;
 	}
 
 	@Override
--- JavaServer/src/main/java/com/example/FantasyFootball/UserController.java
+++ JavaServer/src/main/java/com/example/FantasyFootball/UserController.java
@@ -4,6 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
 
 @RestController
 public class UserController {
@@ -12,8 +19,24 @@ public class UserController {
     private UserRepo repo;
 
     @GetMapping("/api/user")
-    public void register(@RequestParam("username") String username, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("confirmPassword") String confirmPassword) {
-        repo.save(new User(username, email, password));
+    public void register(@RequestParam("username") String username, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("confirmPassword") String confirmPassword) throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        if(!password.equals(confirmPassword)) {
+            System.out.println("Passwords don't match");
+            return;
+        }
+
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = secureRandom.generateSeed(12);
+
+        PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray(), salt, 10, 512);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        byte[] hash = skf.generateSecret(pbeKeySpec).getEncoded();
+
+        String base64Hash = Base64.getMimeEncoder().encodeToString(hash);
+        String base64Salt = Base64.getMimeEncoder().encodeToString(salt);
+
+        repo.save(new User(username, email, base64Hash, base64Salt));
 
         // fetch all customers
         System.out.println("Users found with findAll():");
@@ -27,14 +50,20 @@ public class UserController {
 
 
     @GetMapping("/api/login")
-    public void login(@RequestParam("username") String username, @RequestParam("password") String password) {
+    public void login(@RequestParam("username") String username, @RequestParam("password") String password) throws NoSuchAlgorithmException, InvalidKeySpecException, UnsupportedEncodingException {
         // fetch customer and verify password
-        User userLogin = new User(" "," "," ");
+        User userLogin = new User(" "," "," "," ");
         userLogin = repo.findByUsername(username);
 
-        System.out.println(userLogin.getPassword() + " = " + password);
+        byte[] salt = Base64.getDecoder().decode(new String(userLogin.getSalt()).getBytes("UTF-8"));
 
-        if(userLogin.getPassword().equals(password)) {
+        PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray(), salt, 10, 512);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        byte[] hash = skf.generateSecret(pbeKeySpec).getEncoded();
+
+        String base64Hash = Base64.getMimeEncoder().encodeToString(hash);
+
+        if(base64Hash.equals(userLogin.getPassword())) {
             System.out.println("Login Succesful!");
         }
         else {
