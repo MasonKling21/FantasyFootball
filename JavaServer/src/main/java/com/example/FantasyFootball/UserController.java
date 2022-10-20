06e3a1bf7730fde146ddda027288b688fb2b597f
Mason Kling <klingma@oregonstate.edu>
2022-10-20 16:08:59 -0700

    getHash function created

--- JavaServer/src/main/java/com/example/FantasyFootball/UserController.java
+++ JavaServer/src/main/java/com/example/FantasyFootball/UserController.java
@@ -19,56 +19,59 @@ public class UserController {
     private UserRepo repo;
 
     @GetMapping("/api/user")
-    public void register(@RequestParam("username") String username, @RequestParam("email") String email, @RequestParam("password") String password, @RequestParam("confirmPassword") String confirmPassword) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public User register(
+        @RequestParam("username") String username,
+        @RequestParam("email") String email,
+        @RequestParam("password") String password,
+        @RequestParam("confirmPassword") String confirmPassword)
+        throws 
+        NoSuchAlgorithmException,
+        InvalidKeySpecException,
+        UnsupportedEncodingException {
 
         if(!password.equals(confirmPassword)) {
             System.out.println("Passwords don't match");
-            return;
+            return null;
         }
 
         SecureRandom secureRandom = new SecureRandom();
         byte[] salt = secureRandom.generateSeed(12);
 
-        PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray(), salt, 10, 512);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-        byte[] hash = skf.generateSecret(pbeKeySpec).getEncoded();
-
-        String base64Hash = Base64.getMimeEncoder().encodeToString(hash);
+        String base64Hash = getHash(password, salt);
         String base64Salt = Base64.getMimeEncoder().encodeToString(salt);
 
         repo.save(new User(username, email, base64Hash, base64Salt));
 
         // fetch all customers
+        /*
         System.out.println("Users found with findAll():");
         System.out.println("-------------------------------");
         for (User user : repo.findAll()) {
         System.out.println(user);
         }
         System.out.println();
-        return;
+        */
+        return repo.findByUsername(username);
     }
 
 
     @GetMapping("/api/login")
-    public void login(@RequestParam("username") String username, @RequestParam("password") String password) throws NoSuchAlgorithmException, InvalidKeySpecException, UnsupportedEncodingException {
+    public User login(@RequestParam("username") String username, @RequestParam("password") String password) throws NoSuchAlgorithmException, InvalidKeySpecException, UnsupportedEncodingException {
         // fetch customer and verify password
         User userLogin = repo.findByUsername(username);
 
         byte[] salt = Base64.getDecoder().decode(new String(userLogin.getSalt()).getBytes("UTF-8"));
 
-        PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray(), salt, 10, 512);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-        byte[] hash = skf.generateSecret(pbeKeySpec).getEncoded();
-
-        String base64Hash = Base64.getMimeEncoder().encodeToString(hash);
+        String base64Hash = getHash(password, salt);
 
         if(base64Hash.equals(userLogin.getPassword())) {
             System.out.println("Login Succesful!");
+            return userLogin;
         }
         else {
             System.out.println("Incorrect Password!");
         }
-        return;
+        return null;
     }
 
 
@@ -79,11 +82,7 @@ public class UserController {
 
         byte[] salt = Base64.getDecoder().decode(new String(userLogin.getSalt()).getBytes("UTF-8"));
 
-        PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray(), salt, 10, 512);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-        byte[] hash = skf.generateSecret(pbeKeySpec).getEncoded();
-
-        String base64Hash = Base64.getMimeEncoder().encodeToString(hash);
+        String base64Hash = getHash(password, salt);
 
         if(base64Hash.equals(userLogin.getPassword())) {
             repo.deleteByUsername(userLogin.getUsername());
@@ -93,5 +92,15 @@ public class UserController {
             System.out.println("Error deleting account!");
         }
         return;
+    }
+
+    public String getHash(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException, UnsupportedEncodingException {
+        PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray(), salt, 10, 512);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        byte[] hash = skf.generateSecret(pbeKeySpec).getEncoded();
+
+        String base64Hash = Base64.getMimeEncoder().encodeToString(hash);
+
+        return base64Hash;
     }
 }
